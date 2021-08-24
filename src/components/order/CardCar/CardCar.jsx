65500ede87d9523098eb62e4d254073684b1fb8b
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-08-25 02:43:55 +0300

    V-10 Файл редактирования отд машины

---
+++ src/components/order/CardCar/CardCar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./cardCar.scss";
+import OrderLayout from "../../layouts/OrderLayout/OrderLayout";
+
+const CardCar = () => {
+  return (
+    <OrderLayout>
+      <main>CardCar</main>
+    </OrderLayout>
+  );
+};
+
+export default CardCar;
