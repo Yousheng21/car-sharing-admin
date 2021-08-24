101f3d581b13618ed7b08a57220f073751eb54e8
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-08-25 02:45:16 +0300

    V-09 Страница списка машин

---
+++ src/components/order/CarList/CarList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./carList.scss";
+import OrderLayout from "../../layouts/OrderLayout/OrderLayout";
+
+const CarList = () => {
+  return (
+    <OrderLayout>
+      <main>CarList</main>
+    </OrderLayout>
+  );
+};
+
+export default CarList;
