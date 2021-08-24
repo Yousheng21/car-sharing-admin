da9018ced1026b1a9dd5bc9d0121bfae54abcfb0
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-08-25 02:49:04 +0300

    Корректировка и подкл доб стилей

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle";
 import "./index.scss";
+import { Provider } from "react-redux";
 import App from "./components/App";
+import { store } from "./reducers";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
