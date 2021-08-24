e1a89643e0a633b0d07e903d93a5d42d2f115d8d
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-08-25 02:41:21 +0300

    V-07 Страница авторизации

--- src/components/Authorization/Authorization.jsx
+++ src/components/Authorization/Authorization.jsx
@@ -1,8 +1,72 @@
 import React from "react";
-import "./authorization.sccs";
+import "./authorization.scss";
+
+import { useDispatch, useSelector } from "react-redux";
+import Logo from "../../images/Logo Icon.svg";
+import Input from "../../utils/Input/Input";
+import { useInput } from "../../utils/Validator/validator";
+import { login } from "../../actions/login";
 
 const Authorization = () => {
-  return <div>Authorization</div>;
+  const dispatch = useDispatch();
+
+  const isErrorForm = useSelector((state) => state.user.isErrorAuth);
+
+  const email = useInput("", {
+    isEmpty: { value: true, text: "Пустое поле" },
+  });
+  const password = useInput("", {
+    isEmpty: { value: true, text: "Пустое поле" },
+  });
+  return (
+    <div className="auth">
+      <div className="auth-header">
+        <Logo />
+        <h1>Need for drive</h1>
+      </div>
+      <main className="auth-content">
+        <h2>Вход</h2>
+        <form action="">
+          <Input
+            id="username"
+            value={email.value}
+            onBlur={email.onBlur}
+            placeholder="Введите логин..."
+            type="text"
+            label="Логин"
+            setValue={email.onChange}
+            error={email.isDirty && email.printError(["isEmpty", "emailError"])}
+          />
+          <Input
+            id="password"
+            value={password.value}
+            onBlur={password.onBlur}
+            placeholder="Введите пароль..."
+            type="password"
+            label="Пароль"
+            setValue={password.onChange}
+            error={password.isDirty && password.printError(["isEmpty"])}
+          />
+          <section className="auth-footer">
+            <a className="link-primary" href="/car-sharing-admin">
+              Запросить доступ
+            </a>
+            <button
+              type="button"
+              className="btn btn-primary"
+              disabled={!email.inputValid || !password.inputValid}
+              onClick={() => dispatch(login(email.value, password.value))}
+            >
+              Войти
+            </button>
+          </section>
+        </form>
+        <span className="error">
+          {isErrorForm.value ? isErrorForm.text : ""}
+        </span>
+      </main>
+    </div>
+  );
 };
 
 export default Authorization;
