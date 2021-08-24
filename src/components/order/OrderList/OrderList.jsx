d05e416611be405be6ec564cf9824e8ac036d7e6
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-08-25 02:45:49 +0300

    V-08 Обертывание в лэйаут

--- src/components/order/OrderList/OrderList.jsx
+++ src/components/order/OrderList/OrderList.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./orderList.scss";
+import OrderLayout from "../../layouts/OrderLayout/OrderLayout";
 
 const OrderList = () => {
-  return <div>OrderList</div>;
+  return (
+    <OrderLayout>
+      <main>OrderList</main>
+    </OrderLayout>
+  );
 };
 
 export default OrderList;
