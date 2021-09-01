eb2e2bab20011bcf91061519d6f2d57321a3b5d0
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-09-02 02:52:14 +0300

    F-10 Получение моделей машин

---
+++ src/actions/car.js
@@ -0,0 +1,21 @@
+import { instance } from "../reducers/data/dataServer";
+import { setModels } from "../reducers/appReducer";
+
+const getCarModels = () => {
+  return async (dispatch) => {
+    try {
+      const response = await instance({
+        method: "GET",
+        url: `/api/db/car`,
+        params: {
+          limit: 10,
+        },
+      });
+      dispatch(setModels(response.data.data));
+    } catch (e) {
+      console.error(e.response);
+    }
+  };
+};
+
+export default getCarModels;
