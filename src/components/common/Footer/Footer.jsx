1291c4fd0b0bc24ea5f733ebbf8f7837cdc26cfb
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-09-02 02:56:51 +0300

    V-09 Реализация подвала приложения

--- src/components/common/Footer/Footer.jsx
+++ src/components/common/Footer/Footer.jsx
@@ -2,7 +2,15 @@ import React from "react";
 import "./footer.scss";
 
 const Footer = () => {
-  return <div className="footer">Footer</div>;
+  return (
+    <div className="footer">
+      <section className="footer-main">
+        <a href="/car-sharing-admin">Главная страница</a>
+        <a href="/car-sharing-admin">Ссылка</a>
+      </section>
+      <span>Copyright © 2020 Simbirsoft</span>
+    </div>
+  );
 };
 
 export default Footer;
