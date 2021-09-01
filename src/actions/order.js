ffd1f30ae5388b6ebe335e185995ec3b6460a754
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-09-02 02:54:34 +0300

    F-10 Получение заказа с возможными фильтрами

---
+++ src/actions/order.js
@@ -0,0 +1,42 @@
+import { instance } from "../reducers/data/dataServer";
+import { setNewOrders, setOrders } from "../reducers/appReducer";
+import { store } from "../reducers";
+
+const LIMIT = 10;
+
+const getOrders = (parameters) => {
+  const { accessToken } = store.getState().user.user;
+  return async (dispatch) => {
+    try {
+      const response = await instance({
+        method: "GET",
+        url: `/api/db/order?${
+          parameters
+            ? `${parameters.carId ? `carId=${parameters.carId}&` : ""}${
+                parameters.cityId ? `cityId=${parameters.cityId}&` : ""
+              }${
+                parameters.orderStatusId
+                  ? `orderStatusId=${parameters.orderStatusId}`
+                  : ""
+              }`
+            : ""
+        }`,
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+        params: {
+          limit: LIMIT,
+        },
+      });
+      dispatch(
+        parameters
+          ? setNewOrders(response.data.data)
+          : setOrders(response.data.data)
+      );
+    } catch (e) {
+      console.error(e.response);
+    }
+  };
+};
+
+export default getOrders;
