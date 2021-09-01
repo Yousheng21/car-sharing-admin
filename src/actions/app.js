c99b166cbb3daf11e06e517f5925b9a695710e3c
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-09-02 02:51:47 +0300

    F-10 Реализация полей фильтрации заказов

---
+++ src/actions/app.js
@@ -0,0 +1,41 @@
+import {
+  orderCloseId,
+  orderCompleteId,
+  orderNewId,
+} from "../reducers/data/dataOrder";
+
+export const getDropdown = (storeModels, storeCities) => {
+  const models = storeModels.map((model) => {
+    return { name: model.name, id: model.id };
+  });
+
+  const cities = storeCities.map((city) => {
+    return { name: city.name, id: city.id };
+  });
+
+  return [
+    {
+      name: "dateFrom",
+      options: [
+        { name: "За все время", id: "" },
+        { name: "За неделю", id: "" },
+        { name: "За месяц", id: "" },
+        { name: "За день", id: "" },
+      ],
+    },
+    { name: "carId", options: [{ name: "Все модели", id: "" }, ...models] },
+    {
+      name: "cityId",
+      options: [{ name: "Все города", id: "" }, ...cities],
+    },
+    {
+      name: "orderStatusId",
+      options: [
+        { name: "Все", id: "" },
+        { name: "В процессе", id: orderNewId },
+        { name: "Завершенные", id: orderCompleteId },
+        { name: "Отмененные", id: orderCloseId },
+      ],
+    },
+  ];
+};
