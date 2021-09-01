90bd46e42c68abdad0c1746dffa3ef96423689a1
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-09-02 02:53:04 +0300

    F-10 Получение городов

---
+++ src/actions/city.js
@@ -0,0 +1,21 @@
+import { instance } from "../reducers/data/dataServer";
+import { setCities } from "../reducers/appReducer";
+
+const getCities = () => {
+  return async (dispatch) => {
+    try {
+      const response = await instance({
+        method: "GET",
+        url: `/api/db/city`,
+        params: {
+          limit: 5,
+        },
+      });
+      dispatch(setCities(response.data.data));
+    } catch (e) {
+      console.error(e.response);
+    }
+  };
+};
+
+export default getCities;
