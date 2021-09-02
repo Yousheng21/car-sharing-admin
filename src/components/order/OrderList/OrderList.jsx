1c70ee1edd006637558f87748923b83d0bf5d364
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-09-02 03:06:16 +0300

    V-08 Разделение логики на отд компоненты и доп стили

--- src/components/order/OrderList/OrderList.jsx
+++ src/components/order/OrderList/OrderList.jsx
@@ -1,11 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./orderList.scss";
+import { useDispatch, useSelector } from "react-redux";
 import OrderLayout from "../../layouts/OrderLayout/OrderLayout";
+import getOrders from "../../../actions/order";
+import Filters from "./Filters";
+import Pagination from "../../common/Pagination/Pagination";
+import Order from "./Order";
 
-const OrderList = () => {
+const OrderList = ({ page }) => {
+  const dispatch = useDispatch();
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [orderPerPage] = useState(1);
+
+  const orders = useSelector((state) => state.app.orders);
+  const newOrders = useSelector((state) => state.app.newOrders);
+
+  useEffect(() => {
+    if (!orders.length) dispatch(getOrders());
+  }, [orders.length]);
+
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const indexOfLastOrder = currentPage * orderPerPage;
+  const indexOfFirstOrder = indexOfLastOrder - orderPerPage;
+  const currentOrders = newOrders.slice(indexOfFirstOrder, indexOfLastOrder);
+
   return (
-    <OrderLayout>
-      <main>OrderList</main>
+    <OrderLayout title="Заказы" page={page}>
+      <main className="order-list">
+        <Filters paginate={paginate} />
+        <Order orders={currentOrders} />
+        <Pagination page={currentPage} orders={newOrders} paginate={paginate} />
+      </main>
     </OrderLayout>
   );
 };
