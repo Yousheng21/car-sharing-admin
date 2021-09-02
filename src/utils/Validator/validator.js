4ae5b584b31c1f67160d3432f0ff3ef4e07b65d0
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-09-02 18:40:56 +0300

    F перенос регулярки за пределы функции

--- src/utils/Validator/validator.js
+++ src/utils/Validator/validator.js
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
 
+const regExp =
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
 export const useValidation = (value, validations) => {
   const [isEmpty, setEmpty] = useState({ value: true, text: "" });
   const [emailError, setEmailError] = useState({ value: false, text: "" });
@@ -13,17 +16,13 @@ export const useValidation = (value, validations) => {
           else setEmpty({ value: true, text: validations[validation].text });
           break;
         case "isEmail":
-          {
-            const re =
-              /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-            if (re.test(String(value).toLowerCase()))
-              setEmailError({ value: false, text: "" });
-            else
-              setEmailError({
-                value: true,
-                text: validations[validation].text,
-              });
-          }
+          if (regExp.test(String(value).toLowerCase()))
+            setEmailError({ value: false, text: "" });
+          else
+            setEmailError({
+              value: true,
+              text: validations[validation].text,
+            });
           break;
         default: {
           break;
@@ -41,44 +40,4 @@ export const useValidation = (value, validations) => {
   }, [isEmpty.value, emailError.value]);
 
   return { isEmpty, emailError, inputValid };
-};
-
-export const useInput = (initialState, validations) => {
-  const [value, setValue] = useState(initialState);
-  const [isDirty, setDirty] = useState(false);
-  const valid = useValidation(value, validations);
-
-  const onChange = (e) => {
-    setValue(e.target.value);
-  };
-
-  const onBlur = () => {
-    setDirty(true);
-  };
-  const onFocus = () => {
-    setDirty(false);
-  };
-
-  const printError = (validators) => {
-    let flag = false;
-    return validators.map((item) => {
-      if (!flag) {
-        if (valid[item].value) {
-          flag = true;
-          return valid[item].text;
-        }
-      }
-      return true;
-    });
-  };
-
-  return {
-    value,
-    onChange,
-    onBlur,
-    onFocus,
-    isDirty,
-    printError,
-    ...valid,
-  };
 };
