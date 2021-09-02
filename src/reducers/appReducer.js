0ef4c28d74038ba159237e307fcd2fa1aab3a4a6
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-09-02 03:17:34 +0300

    F-09 Добавление состояния заказов и моделей машин

--- src/reducers/appReducer.js
+++ src/reducers/appReducer.js
@@ -1,22 +1,59 @@
-const SET_CURRENT_PAGE = "SET_CURRENT_PAGE";
+const SET_ORDERS = "SET_ORDERS";
+const SET_ORDERS_NEW = "SET_ORDERS_NEW";
+const SET_MODELS = "SET_MODELS";
+const SET_CITIES = "SET_CITIES";
 
 const defaultState = {
-  currentPage: 0,
+  orders: [],
+  newOrders: [],
+  models: [],
+  cities: [],
 };
 
 export default function appReducer(state = defaultState, action) {
   switch (action.type) {
-    case SET_CURRENT_PAGE:
+    case SET_ORDERS:
       return {
         ...state,
-        currentPage: action.page,
+        orders: action.orders,
+        newOrders: action.orders,
+      };
+    case SET_ORDERS_NEW:
+      return {
+        ...state,
+        newOrders: action.orders,
+      };
+    case SET_MODELS:
+      return {
+        ...state,
+        models: action.models,
+      };
+    case SET_CITIES:
+      return {
+        ...state,
+        cities: action.cities,
       };
     default:
       return state;
   }
 }
 
-export const setCurrentPage = (page) => ({
-  type: SET_CURRENT_PAGE,
-  page,
+export const setOrders = (orders) => ({
+  type: SET_ORDERS,
+  orders,
+});
+
+export const setNewOrders = (orders) => ({
+  type: SET_ORDERS_NEW,
+  orders,
+});
+
+export const setModels = (models) => ({
+  type: SET_MODELS,
+  models,
+});
+
+export const setCities = (cities) => ({
+  type: SET_CITIES,
+  cities,
 });
