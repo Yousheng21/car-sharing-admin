fa0f83b7128b7bc746901f3c1c8f9194a8757e19
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-09-02 03:16:37 +0300

    F Информация о заказе

---
+++ src/reducers/data/dataOrder.js
@@ -0,0 +1,9 @@
+export const orderCompleteId = "5e26a1f0099b810b946c5d8b";
+export const orderNewId = "5e26a191099b810b946c5d89";
+export const orderCloseId = "5e26a1f5099b810b946c5d8c";
+
+export const additional = [
+  { name: "Полный бак", key: "isFullTank" },
+  { name: "Детское кресло", key: "isNeedChildChair" },
+  { name: "Правый руль", key: "isRightWheel" },
+];
