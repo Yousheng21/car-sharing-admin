50ca4f80d76e1969b365a017e0f55276c43b1ad7
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-09-02 03:14:53 +0300

    F Доб функции переавторизации и передача текущей вкладки

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,42 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Authorization from "./Authorization/Authorization";
 import OrderList from "./order/OrderList/OrderList";
 import CardCar from "./order/CardCar/CardCar";
 import CarList from "./order/CarList/CarList";
+import { auth } from "../actions/login";
 
 const App = () => {
+  const dispatch = useDispatch();
   const isAuth = useSelector((state) => state.user.isAuth);
+  const token = localStorage.getItem("token");
+
+  useEffect(() => {
+    if (token) dispatch(auth());
+  }, [token]);
+
   return (
     <BrowserRouter>
       {!isAuth ? (
         <Switch>
-          <Route exact path="/car-sharing-admin" component={Authorization} />
+          <Route path="/car-sharing-admin" component={Authorization} />
         </Switch>
       ) : (
         <Switch>
-          <Route exact path="/car-sharing-admin" component={CardCar} />
-          <Route path="/car-sharing-admin/cardList" component={CarList} />
-          <Route path="/car-sharing-admin/orderList" component={OrderList} />
+          <Route
+            exact
+            path="/car-sharing-admin"
+            render={() => <CardCar page={0} />}
+          />
+          <Route
+            path="/car-sharing-admin/cardList"
+            render={() => <CarList page={1} />}
+          />
+          <Route
+            path="/car-sharing-admin/orderList"
+            render={() => <OrderList page={2} />}
+          />
         </Switch>
       )}
     </BrowserRouter>
