19a7b2e10f25a8633eecc4cd2f4b62def8944780
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-09-03 05:07:13 +0300

    F-10 Добавление фильтрации даты

--- src/actions/app.js
+++ src/actions/app.js
@@ -12,15 +12,24 @@ export const getDropdown = (storeModels, storeCities) => {
   const cities = storeCities.map((city) => {
     return { name: city.name, id: city.id };
   });
+  const day = new Date();
+  day.setHours(0);
+  day.setMinutes(0);
+
+  const week = new Date(day);
+  week.setDate(week.getDate() - 7);
+
+  const month = new Date(day);
+  month.setMonth(month.getMonth() - 1);
 
   return [
     {
-      name: "dateFrom",
+      name: "createdAt[$gt]",
       options: [
         { name: "За все время", id: "" },
-        { name: "За неделю", id: "" },
-        { name: "За месяц", id: "" },
-        { name: "За день", id: "" },
+        { name: "За месяц", id: month.getTime() },
+        { name: "За неделю", id: week.getTime() },
+        { name: "За день", id: day.getTime() },
       ],
     },
     { name: "carId", options: [{ name: "Все модели", id: "" }, ...models] },
