1a92024f685d5ff3d708f18afeaaf8b994d63bce
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-09-05 19:29:45 +0300

    V-09 Компонента списка машин

---
+++ src/components/CarList/Car.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Car = () => {
+  return <section />;
+};
+
+export default Car;
--- src/components/CarList/CarList.jsx
+++ src/components/CarList/CarList.jsx
@@ -1,11 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./carList.scss";
+import { useDispatch, useSelector } from "react-redux";
 import AppLayout from "../layouts/AppLayout/AppLayout";
+import Order from "../OrderList/Order/Order";
+import EntitiesLayout from "../layouts/EntitiesLayout/EntitiesLayout";
+import getCarModels, { getCategories } from "../../actions/car";
+import { getDropdownCar } from "../../actions/app";
 
 const CarList = ({ page }) => {
+  const dispatch = useDispatch();
+
+  const [modelsPerPage] = useState(5);
+  const [dropdown, setDropdown] = useState([]);
+  const [dataForm, setDataForm] = useState({
+    id: "",
+    categoryId: "",
+    "priceMax[$lt]": "",
+    "priceMin[$gt]": "",
+    colors: "",
+  });
+  const models = useSelector((state) => state.app.models);
+  const newModels = useSelector((state) => state.app.models);
+  const categories = useSelector((state) => state.app.categories);
+
+  const [currentModels, setCurrentModels] = useState(
+    models.slice(0, modelsPerPage)
+  );
+
+  useEffect(() => {
+    if (!models.length) {
+      dispatch(getCarModels());
+    } else setCurrentModels(models.slice(0, modelsPerPage));
+  }, [models.length]);
+
+  useEffect(() => {
+    if (!categories.length) dispatch(getCategories());
+    else setDropdown(getDropdownCar(models, categories));
+  }, [categories.length]);
+
+  const handleClick = (filters) => {
+    dispatch(getCarModels(filters));
+  };
   return (
     <AppLayout title="Список авто" page={page}>
-      <main>CarList</main>
+      <EntitiesLayout
+        className="car-list"
+        dropdown={dropdown}
+        dataForm={dataForm}
+        entities={newModels}
+        setEntities={setCurrentModels}
+        handleClick={handleClick}
+        setDataForm={setDataForm}
+        orderPerPage={modelsPerPage}
+      >
+        <Order storeModels={models} models={currentModels} />
+      </EntitiesLayout>
     </AppLayout>
   );
 };
