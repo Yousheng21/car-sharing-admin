19ae4a51df6425138ee4729a49518c0138d18d10
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-09-06 04:26:07 +0300

    F Удаление лишнего кода

--- src/reducers/data/dataOrder.js
+++ src/reducers/data/dataOrder.js
@@ -1,7 +1,3 @@
-export const orderCompleteId = "5e26a1f0099b810b946c5d8b";
-export const orderNewId = "5e26a191099b810b946c5d89";
-export const orderCloseId = "5e26a1f5099b810b946c5d8c";
-
 export const additional = [
   { name: "Полный бак", key: "isFullTank" },
   { name: "Детское кресло", key: "isNeedChildChair" },
