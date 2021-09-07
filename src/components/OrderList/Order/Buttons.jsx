80f6dfd168e6062ffb26c72b5ac1b9deb1943466
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-09-07 06:38:58 +0300

    V-08 Вывод всех возможных кнопок

--- src/components/OrderList/Order/Buttons.jsx
+++ src/components/OrderList/Order/Buttons.jsx
@@ -1,29 +1,17 @@
 import React from "react";
-import {
-  orderCloseId,
-  orderCompleteId,
-} from "../../../reducers/data/api/server";
 import Check from "../../../images/Check Icon.svg";
 import Reject from "../../../images/Reject Icon.svg";
 import Edit from "../../../images/Edit Icon.svg";
 
-const Buttons = ({ order }) => {
+const Buttons = () => {
   return (
     <div className="buttons">
-      {order.orderStatusId && orderCompleteId === order.orderStatusId.id ? (
-        ""
-      ) : (
-        <button type="button">
-          <Check /> Готово
-        </button>
-      )}
-      {order.orderStatusId && orderCloseId === order.orderStatusId.id ? (
-        ""
-      ) : (
-        <button type="button">
-          <Reject /> Отмена
-        </button>
-      )}
+      <button type="button">
+        <Check /> Готово
+      </button>
+      <button type="button">
+        <Reject /> Отмена
+      </button>
       <button type="button">
         <Edit /> Изменить
       </button>
