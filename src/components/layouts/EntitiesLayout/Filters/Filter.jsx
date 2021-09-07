ea8a1b56cb8fbe706f655631cb83174ff59d9a0c
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-09-07 06:38:01 +0300

    F-10 Улучшение управление значениями фильтров для дальнейшего сброса

--- src/components/layouts/EntitiesLayout/Filters/Filter.jsx
+++ src/components/layouts/EntitiesLayout/Filters/Filter.jsx
@@ -1,15 +1,23 @@
-import React, { useState } from "react";
+import React from "react";
+import { objColors } from "../../../../reducers/data/dataColors";
 
-const Filter = ({ item, handleChange }) => {
-  const [input, setInput] = useState("");
-
+const Filter = ({ item, handleChange, dataForm }) => {
   switch (item.type) {
     case "select":
       return (
         <div className="select">
-          <select name={item.name} onChange={handleChange} id={item.name}>
+          <select
+            value={dataForm[item.name]}
+            name={item.name}
+            onChange={handleChange}
+            id={item.name}
+          >
             {item.options.map((el) => (
-              <option key={el.id} value={el.id}>
+              <option
+                style={{ backgroundColor: objColors[el.name] ?? "" }}
+                key={el.id}
+                value={el.id}
+              >
                 {el.name}
               </option>
             ))}
@@ -23,11 +31,8 @@ const Filter = ({ item, handleChange }) => {
         <input
           className="number"
           type="number"
-          onChange={(event) => {
-            handleChange(event);
-            setInput(event.target.value);
-          }}
-          value={input}
+          onChange={handleChange}
+          value={dataForm[item.name]}
           name={item.name}
           id={item.name}
           placeholder={item.text}
