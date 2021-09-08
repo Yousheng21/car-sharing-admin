5f5429944c2942beb3c8aa612d9a42f57299d8a9
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-09-08 06:37:59 +0300

    F Добавление вкладки пунктов выдачи

--- src/reducers/data/dataMenu.js
+++ src/reducers/data/dataMenu.js
@@ -1,10 +1,12 @@
 import React from "react";
 import BlogIcon from "../../images/Blog Icon.svg";
-import BlogPost from "../../images/Blog Posts Icon.svg";
-import NewPost from "../../images/Add New Post Icon.svg";
+import CarIcon from "../../images/CarIcon.svg";
+import PointIcon from "../../images/Point.svg";
+import OrderIcon from "../../images/OrderIcon.svg";
 
 export const tabs = [
   { text: "Карточка автомобиля", icon: <BlogIcon />, path: "/" },
-  { text: "Список авто", icon: <BlogPost />, path: "/cardList" },
-  { text: "Заказы", icon: <NewPost />, path: "/orderList" },
+  { text: "Список авто", icon: <CarIcon />, path: "/cardList" },
+  { text: "Заказы", icon: <OrderIcon />, path: "/orderList" },
+  { text: "Пункты", icon: <PointIcon />, path: "/pointList" },
 ];
