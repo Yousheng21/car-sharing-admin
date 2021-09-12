8485eb1b41d78e5758ec0c4575cb77c931d4be79
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-09-13 00:39:46 +0300

    F-10 Отд хранение объекта машины для отправления на сервер

---
+++ src/reducers/data/dataCar.js
@@ -0,0 +1,13 @@
+export const dataFormCar = {
+  priceMax: 0,
+  priceMin: 0,
+  name: "",
+  thumbnail: {},
+  description: "",
+  tank: 0,
+  categoryId: {
+    name: "",
+    id: "",
+  },
+  colors: [],
+};
