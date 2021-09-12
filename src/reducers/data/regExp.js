ac338c1e317708da2a0404d2681767ae12091178
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-09-13 00:40:14 +0300

    F-10 Доб регулярных выражений

--- src/reducers/data/regExp.js
+++ src/reducers/data/regExp.js
@@ -1,2 +1,6 @@
 export const regExpEmail =
   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+export const regExpModelName = /[a-zA-Z0-9/\s]{4,}/;
+export const regExpColor = /^[А-яЁё/\s]{3,}$/;
+export const regExpTab = /\s+/g;
+export const regExpLocalImage = /^C:\\fakepath\\/;
