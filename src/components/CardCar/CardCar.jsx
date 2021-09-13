07f9944c5ee50ee433858f0d299036217537fcd3
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-09-14 01:37:20 +0300

    F-09 Представление параметров машины как объекта для упрощения и улучшения логики

--- src/components/CardCar/CardCar.jsx
+++ src/components/CardCar/CardCar.jsx
@@ -5,93 +5,115 @@ import AppLayout from "../layouts/AppLayout/AppLayout";
 import Settings from "./Settings/Settings";
 import getCarModels, { getCategories } from "../../actions/car";
 import Card from "./Card";
-import Preloader from "../common/Preloader/Preloader";
+// import Preloader from "../common/Preloader/Preloader";
 import RefactorEntitiesLayout from "../layouts/RefactorEntitiesLayout/RefactorEntitiesLayout";
 import { useInput } from "../../utils/Validator/useInput";
-import { dataFormCar } from "../../reducers/data/dataCar";
 
 const CardCar = ({ page }) => {
   const dispatch = useDispatch();
 
-  const [stateMax, setStateMax] = useState(1);
-  const [colors, setColors] = useState([]);
-
-  const modelName = useInput(dataFormCar.name, {
-    isEmpty: { value: false, text: "Пустое поле" },
-    isModelName: {
-      value: false,
-      text: "Введите от 4-х символов латиницы или цифр",
-    },
-  });
-  const currColor = useInput("", {
-    isColor: { value: false, text: "только кириллица от 3-х символов" },
-  });
-  const priceMin = useInput(dataFormCar.priceMin, {
-    minError: { value: false, text: "Введите натуральное число", min: 0 },
-    maxError: {
-      value: false,
-      text: "Введие число меньше макс цены",
-      max: stateMax,
-    },
-  });
-  const priceMax = useInput(dataFormCar.priceMax, {
-    isEmpty: {
-      value: false,
-      text: "Пустое поле",
-    },
-    minError: {
-      value: false,
-      text: "Введие число больше мин цены",
-      min: priceMin.value,
-    },
-  });
-  const description = useInput(dataFormCar.description, {});
-  const tank = useInput(dataFormCar.tank, {});
-  const [category, setCategory] = useState(dataFormCar.categoryId);
+  const dataFormCar = useSelector((state) => state.app.dataFormCar);
 
   const car = useSelector((state) => state.app.models[0]);
   const categories = useSelector((state) => state.app.categories);
 
+  const [stateMax, setStateMax] = useState(1);
+  const [stateMin, setStateMin] = useState(0);
+
+  const [colors, setColors] = useState({
+    value: dataFormCar.colors,
+    inputValid: dataFormCar.colors.length,
+  });
+  const [thumbnail, setThumbnail] = useState({
+    value: {
+      name: dataFormCar.thumbnail.name,
+      path: dataFormCar.thumbnail.path,
+    },
+    inputValid: dataFormCar.thumbnail.path,
+  });
+
+  const dataForm = {
+    name: useInput(dataFormCar.name, {
+      isEmpty: { value: false, text: "Пустое поле" },
+      isModelName: {
+        value: false,
+        text: "Введите от 4-х символов латиницы или цифр",
+      },
+    }),
+    priceMin: useInput(dataFormCar.priceMin, {
+      minError: { value: false, text: "Введите натуральное число", min: 0 },
+      maxError: {
+        value: false,
+        text: "Введие число меньше макс цены",
+        max: stateMax,
+      },
+    }),
+    priceMax: useInput(dataFormCar.priceMax, {
+      isEmpty: {
+        value: false,
+        text: "Пустое поле",
+      },
+      minError: {
+        value: false,
+        text: "Введие число больше мин цены",
+        min: stateMin,
+      },
+    }),
+    thumbnail: {
+      value: thumbnail.value,
+      setValue: setThumbnail,
+      inputValid: thumbnail.inputValid,
+    },
+    description: useInput(dataFormCar.description, {}),
+    tank: useInput(dataFormCar.tank, {}),
+    categoryId: useInput(dataFormCar.categoryId.id, {
+      isEmpty: { value: false, text: "" },
+    }),
+    colors: {
+      value: colors.value,
+      setValue: setColors,
+      inputValid: colors.inputValid,
+    },
+  };
+
+  const handleDataForm = (name, value, valid) => {
+    dataForm[name].setValue({
+      ...dataForm[name],
+      value,
+      inputValid: !!valid,
+    });
+  };
+
+  const handleSave = () => {
+    console.log(dataForm);
+  };
+
   useEffect(() => {
-    setStateMax(priceMax.value);
-  }, [priceMax.value]);
+    setStateMax(dataForm.priceMax.value);
+  }, [dataForm.priceMax.value]);
+
+  useEffect(() => {
+    setStateMin(dataForm.priceMin.value);
+  }, [dataForm.priceMin.value]);
 
   useEffect(() => {
     if (!car) dispatch(getCarModels());
     if (!categories.length) dispatch(getCategories());
   }, [car, categories.length]);
-
-  if (!car || !categories.length) return <Preloader title="Загрузка..." />;
+  // if (!car || !categories.length) return <Preloader title="Загрузка..." />;
   return (
     <AppLayout title="Карточка автомобиля" page={page}>
-      <RefactorEntitiesLayout
-        arrValid={[
-          modelName.inputValid,
-          currColor.inputValid,
-          category.id,
-          priceMin.inputValid,
-          priceMax.inputValid,
-          colors.length,
-        ]}
-      >
+      <RefactorEntitiesLayout dataForm={dataForm} handleSave={handleSave}>
         <section className="card-car">
           <Card
-            tank={tank}
-            description={description}
-            category={category}
-            modelName={modelName}
+            categories={categories}
+            dataForm={dataForm}
+            handleDataForm={handleDataForm}
           />
           <Settings
-            currColor={currColor}
-            colors={colors}
-            setColors={setColors}
-            priceMin={priceMin}
-            priceMax={priceMax}
-            category={category}
-            setCategory={setCategory}
-            car={car}
+            dataForm={dataForm}
+            handleDataForm={handleDataForm}
             categories={categories}
-            modelName={modelName}
           />
         </section>
       </RefactorEntitiesLayout>
