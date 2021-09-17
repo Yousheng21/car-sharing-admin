dbdcff036675d629252788199117c6ba19dd3145
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-09-17 18:46:39 +0300

    F-09 Реализация функции удаление/изменение/добавление машины

--- src/actions/car.js
+++ src/actions/car.js
@@ -1,11 +1,14 @@
 import { instance } from "../reducers/data/api/server";
 import {
+  setCarModelId,
   setCategories,
   setIsUpdated,
   setModels,
   setNewModels,
+  setTooltip,
 } from "../reducers/appReducer";
 import { getUrl } from "./app";
+import { store } from "../reducers";
 
 const getCarModels = (parameters) => {
   return async (dispatch) => {
@@ -38,6 +41,43 @@ export const getCategories = () => {
       console.error(e.response);
     }
   };
+};
+
+export const requestCarModel = (method, req, id) => {
+  const { accessToken } = store.getState().user.user;
+  return async (dispatch) => {
+    try {
+      const response = await instance({
+        method,
+        url: `/api/db/car/${id || ""}`,
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+        data: req,
+      });
+      if (!id) dispatch(setCarModelId(response.data.data.id));
+      dispatch(setIsUpdated(true));
+      dispatch(getCarModels());
+      dispatch(setTooltip("success", method));
+    } catch (e) {
+      dispatch(setTooltip("error", ""));
+      console.error(e.response);
+    }
+  };
+};
+
+export const getCarModelObj = (method, obj, id) => {
+  let result = { ...obj };
+  const handleObjCar = (key) => {
+    result = {
+      ...result,
+      [key]: obj[key].value,
+    };
+  };
+  Object.keys(obj).forEach((item) => {
+    return handleObjCar(item);
+  });
+  store.dispatch(requestCarModel(method, result, id));
 };
 
 export default getCarModels;
