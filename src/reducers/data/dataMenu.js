240c6617772eaf2c7ca3b02a5995dd60aa870cbe
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-09-17 19:23:52 +0300

    F Изменение пути к компоненте CardCar

--- src/reducers/data/dataMenu.js
+++ src/reducers/data/dataMenu.js
@@ -5,7 +5,7 @@ import PointIcon from "../../images/Point.svg";
 import OrderIcon from "../../images/OrderIcon.svg";
 
 export const tabs = [
-  { text: "Карточка автомобиля", icon: <BlogIcon />, path: "/" },
+  { text: "Карточка автомобиля", icon: <BlogIcon />, path: "/cardCar" },
   { text: "Список авто", icon: <CarIcon />, path: "/cardList" },
   { text: "Заказы", icon: <OrderIcon />, path: "/orderList" },
   { text: "Пункты", icon: <PointIcon />, path: "/pointList" },
