69c6cd1d3d70d927384a800068bf74dcf84ec88a
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-09-17 19:18:27 +0300

    F-09 Доб компонента кнопок

--- src/components/layouts/RefactorEntitiesLayout/RefactorEntitiesLayout.jsx
+++ src/components/layouts/RefactorEntitiesLayout/RefactorEntitiesLayout.jsx
@@ -1,32 +1,33 @@
 import React from "react";
 import "./refactorEntitiesLayout.scss";
+import Preloader from "../../common/Preloader/Preloader";
+import RefactorEntitiesButtons from "./RefactorEntitiesButtons";
 
-const RefactorEntitiesLayout = ({ children, dataForm, handleSave }) => {
+const RefactorEntitiesLayout = ({
+  children,
+  dataForm,
+  handleRequest,
+  load,
+  handleReset,
+  id,
+  link,
+}) => {
   const handleDisable = () => {
     return Object.keys(dataForm).some((item) => {
       return !dataForm[item].inputValid;
     });
   };
-
+  if (load) return <Preloader title="Загрузка..." />;
   return (
     <section className="refactor-entities">
       {children}
-      <div className="refactor-entities__buttons">
-        <button
-          disabled={handleDisable()}
-          onClick={handleSave}
-          className="edit"
-          type="button"
-        >
-          Сохранить
-        </button>
-        <button className="back" type="button">
-          Отменить
-        </button>
-        <button className="delete" type="button">
-          Удалить
-        </button>
-      </div>
+      <RefactorEntitiesButtons
+        handleDisable={handleDisable}
+        id={id}
+        handleRequest={handleRequest}
+        handleReset={handleReset}
+        link={link}
+      />
     </section>
   );
 };
