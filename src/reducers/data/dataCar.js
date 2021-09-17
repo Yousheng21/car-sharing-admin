ee3f2323c67b9c39f40f67a36f9e75d1fa29385f
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-09-17 19:22:58 +0300

    F-09 Изменение шаблона сущности машины

--- src/reducers/data/dataCar.js
+++ src/reducers/data/dataCar.js
@@ -2,7 +2,13 @@ export const dataFormCar = {
   priceMax: 0,
   priceMin: 0,
   name: "",
-  thumbnail: {},
+  thumbnail: {
+    originalname: "Выберите изображение...",
+    mimetype: "",
+    size: "",
+    path: "",
+    localPath: "",
+  },
   description: "",
   tank: 0,
   categoryId: {
