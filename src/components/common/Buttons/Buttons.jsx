7bd5a0001f0bec334d89b27697f6b587e578853f
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-09-17 19:06:42 +0300

    А-09 Представление кнопки в виде ссылки

--- src/components/common/Buttons/Buttons.jsx
+++ src/components/common/Buttons/Buttons.jsx
@@ -1,14 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Reject from "../../../images/Reject Icon.svg";
 import Edit from "../../../images/Edit Icon.svg";
 import "./buttons.scss";
 
-const Buttons = () => {
+const Buttons = ({ link }) => {
   return (
     <div className="buttons">
-      <button type="button">
-        <Edit />
-      </button>
+      <Link to={`/car-sharing-admin/${link}`}>
+        <button type="button">
+          <Edit />
+        </button>
+      </Link>
       <button type="button">
         <Reject />
       </button>
