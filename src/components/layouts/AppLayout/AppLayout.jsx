71cc59382c7169cbbf3e0eba073c25c46bfd75c9
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-09-17 19:12:03 +0300

    F Реализация выпадающей информации при отправке запроса

--- src/components/layouts/AppLayout/AppLayout.jsx
+++ src/components/layouts/AppLayout/AppLayout.jsx
@@ -1,16 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./appLayout.scss";
+import classNames from "classnames";
+import { useDispatch, useSelector } from "react-redux";
 import NavBar from "../../common/NavBar/NavBar";
 import Header from "../../common/Header/Header";
 import Footer from "../../common/Footer/Footer";
+import { setTooltip } from "../../../reducers/appReducer";
+import Check from "../../../images/Check Icon.svg";
+import TooltipText from "./TooltipText";
+import Delete from "../../../images/Delete.svg";
 
-const AppLayout = ({ children, title, page }) => {
+const AppLayout = ({ children, title, page, entity, kind, id }) => {
+  const dispatch = useDispatch();
+
+  const [viewTooltip, setViewTooltip] = useState(!!id);
+
+  const tooltip = useSelector((state) => state.app.tooltip);
+  const currModelId = useSelector((state) => state.app.curModelId);
+
+  useEffect(() => {
+    if (tooltip.type) {
+      setTimeout(() => dispatch(setTooltip("", "")), 3000);
+      setViewTooltip(false);
+    }
+  }, [tooltip]);
+
+  const classNameTooltip = classNames({
+    tooltip: true,
+    success: tooltip.type === "success",
+    error: tooltip.type === "error",
+    closed: !tooltip.type,
+  });
+
   return (
-    <div className="order-layout">
+    <div
+      className={classNames({
+        "order-layout": true,
+        edit: viewTooltip,
+      })}
+    >
       <NavBar page={page} />
       <Header />
       <section className="main">
         <h1 className="title">{title}</h1>
+        <div className={classNameTooltip}>
+          <span>
+            <Check />
+            <TooltipText
+              entity={entity}
+              kind={kind}
+              tooltip={tooltip}
+              id={id ?? currModelId}
+            />
+          </span>
+          <button type="button" onClick={() => dispatch(setTooltip("", ""))}>
+            <Delete />
+          </button>
+        </div>
         {children}
       </section>
       <Footer />
