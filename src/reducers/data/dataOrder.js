38426f6c91baad76fa5e1c38a140e1693de5defd
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-09-20 17:29:51 +0300

    F-09 Доб шаблона объкта заказа

--- src/reducers/data/dataOrder.js
+++ src/reducers/data/dataOrder.js
@@ -11,3 +11,35 @@ export const arrOrderStatus = [
   { name: "Завершенные", id: orderCompleteId },
   { name: "Отмененные", id: orderCloseId },
 ];
+
+export const dataFormOrder = {
+  orderStatusId: {
+    id: "",
+    name: "",
+  },
+  cityId: {
+    name: "",
+    id: "",
+  },
+  pointId: {
+    name: "",
+    id: "",
+  },
+  carId: {
+    thumbnail: null,
+    name: "",
+    id: "",
+    colors: "",
+  },
+  color: "",
+  dateFrom: 0,
+  dateTo: 0,
+  rateId: {
+    price: 0,
+    id: "",
+  },
+  price: 0,
+  isFullTank: false,
+  isNeedChildChair: false,
+  isRightWheel: false,
+};
