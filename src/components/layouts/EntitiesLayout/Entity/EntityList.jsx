15d1b57b22320b45d36dac5776492687ed554fe8
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-09-20 17:22:00 +0300

    F-10 Реализация модального окна

--- src/components/layouts/EntitiesLayout/Entity/EntityList.jsx
+++ src/components/layouts/EntitiesLayout/Entity/EntityList.jsx
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./entity.scss";
 import Buttons from "../../../common/Buttons/Buttons";
+import Modal from "../../../common/Modal/Modal";
 
-const EntityList = ({ entity, children }) => {
-  // const [modalIsActive, setModalIsActive] = useState(false);
+const EntityList = ({ entity, children, handleDelete }) => {
+  const [modalIsActive, setModalIsActive] = useState(false);
   const childrenWithExtraProp = React.Children.map(children, (child) => {
     return React.cloneElement(child, { entity });
   });
   return (
     <section className="list-entity">
       {childrenWithExtraProp}
-      <Buttons link={`cardCar/${entity.id}`} />
+      <Buttons setModal={setModalIsActive} link={`cardCar/${entity.id}`} />
+      <Modal active={modalIsActive}>
+        <h1>Подтвердите удаление</h1>
+        <div>
+          <button type="button" onClick={() => setModalIsActive(false)}>
+            Отменить
+          </button>
+          <button
+            onClick={() => handleDelete(entity.id)}
+            className="delete"
+            type="button"
+          >
+            Удалить
+          </button>
+        </div>
+      </Modal>
     </section>
   );
 };
