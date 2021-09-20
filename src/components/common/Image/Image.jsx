a1f6487998870eae366d6b6f13d57f7f1528e12a
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-09-20 22:55:19 +0300

    F-09/F-08 Исп usMemo для упрощение логики

--- src/components/common/Image/Image.jsx
+++ src/components/common/Image/Image.jsx
@@ -1,21 +1,23 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { domain } from "../../../reducers/data/api/server";
 import Photo from "../../../images/no-image.png";
 import "./image.scss";
 
 const Image = ({ car }) => {
-  const getSrc = () => {
-    return car.thumbnail.path[0] === "/"
-      ? domain + car.thumbnail.path
-      : car.thumbnail.path;
-  };
+  const getSrc = useMemo(() => {
+    if (car)
+      return car.thumbnail.path[0] === "/"
+        ? domain + car.thumbnail.path
+        : car.thumbnail.path;
+    return Photo;
+  }, [car]);
 
   return (
     <img
       crossOrigin="anonymous"
       referrerPolicy="origin"
       className="car-img"
-      src={car ? getSrc() : Photo}
+      src={getSrc}
       alt={car ? car.name : "photo"}
     />
   );
