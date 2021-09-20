23be07dabd289e2a37eefdbbdf0afea0d1af0e95
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-09-20 17:20:51 +0300

    F-10 Доб функции открытия модального окна

--- src/components/common/Buttons/Buttons.jsx
+++ src/components/common/Buttons/Buttons.jsx
@@ -4,7 +4,7 @@ import Reject from "../../../images/Reject Icon.svg";
 import Edit from "../../../images/Edit Icon.svg";
 import "./buttons.scss";
 
-const Buttons = ({ link }) => {
+const Buttons = ({ setModal, link }) => {
   return (
     <div className="buttons">
       <Link to={`/car-sharing-admin/${link}`}>
@@ -12,7 +12,7 @@ const Buttons = ({ link }) => {
           <Edit />
         </button>
       </Link>
-      <button type="button">
+      <button onClick={() => setModal(true)} type="button">
         <Reject />
       </button>
     </div>
