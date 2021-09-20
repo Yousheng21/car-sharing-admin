3f0e5fa003bd97ee72d06bd819aecd97f415d1fb
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-09-20 17:14:35 +0300

    F-09 Доб функции получения тарифов из базы

---
+++ src/actions/tariff.js
@@ -0,0 +1,18 @@
+import { instance } from "../reducers/data/api/server";
+import { setTariffs } from "../reducers/appReducer";
+
+const getTariffs = () => {
+  return async (dispatch) => {
+    try {
+      const response = await instance({
+        method: "GET",
+        url: "/api/db/rate",
+      });
+      dispatch(setTariffs(response.data.data));
+    } catch (e) {
+      console.error(e.response);
+    }
+  };
+};
+
+export default getTariffs;
