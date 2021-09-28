0e655e6984c1750811f57f984d6da78a034102c6
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-09-28 03:34:35 +0300

    F Доб регулярки адреса пункта

--- src/reducers/data/regExp.js
+++ src/reducers/data/regExp.js
@@ -2,4 +2,5 @@ export const regExpEmail =
   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 export const regExpModelName = /^[a-zA-Z0-9/\s+(),-.]{4,}$/;
 export const regExpColor = /^[А-яЁё/\s]{3,}$/;
+export const regExpAddress = /^[А-яЁё/\s,0-9"]{3,}$/;
 export const regExpTab = /\s+/g;
