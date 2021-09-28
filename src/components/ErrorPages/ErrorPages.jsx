a1c0cc89562f372a881bcfef550d985d3e8313e9
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-09-28 03:29:00 +0300

    V-11/F Создание компонент ошибок

---
+++ src/components/ErrorPages/ErrorPages.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import AppLayout from "../layouts/AppLayout/AppLayout";
+import "./errorPages.scss";
+
+const ErrorPages = ({ children }) => {
+  return <AppLayout>{children}</AppLayout>;
+};
+
+export default ErrorPages;
