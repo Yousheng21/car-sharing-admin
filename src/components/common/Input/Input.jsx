a5e406949a3c7335c52864d97999ae077cf0c23f
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-09-28 03:24:41 +0300

    F-09 Доб логики

--- src/components/common/Input/Input.jsx
+++ src/components/common/Input/Input.jsx
@@ -1,34 +1,36 @@
 import React from "react";
+import classNames from "classnames";
 import "./input.scss";
 
-const Input = ({
-  value,
-  setValue,
-  type,
-  placeholder,
-  onBlur,
-  onFocus,
-  id,
-  label,
-  error,
-}) => {
+const Input = ({ title, objInput, arrValid, id, type, necessarily }) => {
   return (
-    <div className="input">
-      <label className="form-label" htmlFor={id}>
-        {label}
+    <div
+      className={classNames({
+        input: true,
+        error: (objInput.value || objInput.isDirty) && !objInput.inputValid,
+      })}
+    >
+      <label htmlFor={id}>
+        {necessarily ? (
+          <h6 className="necess" title="обязательное поле">
+            {title} *
+          </h6>
+        ) : (
+          title
+        )}
+        <input
+          onChange={objInput.onChange}
+          onBlur={objInput.onBlur}
+          onFocus={objInput.onFocus}
+          type={type}
+          value={objInput.value}
+          name={id}
+          id={id}
+        />
       </label>
-      <input
-        className="form-control"
-        onChange={setValue}
-        onBlur={onBlur}
-        onFocus={onFocus}
-        value={value}
-        type={type}
-        placeholder={placeholder}
-        id={id}
-        autoComplete="off"
-      />
-      <span className="error">{error ?? ""}</span>
+      <span>
+        {(objInput.value || objInput.isDirty) && objInput.printError(arrValid)}
+      </span>
     </div>
   );
 };
