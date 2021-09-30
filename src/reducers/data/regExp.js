cd865aa3fa026521cac3a9f05ea9f99979814571
Yousheng21 <70286880+Yousheng21@users.noreply.github.com>
2021-10-01 01:55:36 +0300

    F Изменение регулярок

--- src/reducers/data/regExp.js
+++ src/reducers/data/regExp.js
@@ -1,6 +1,6 @@
 export const regExpEmail =
   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-export const regExpModelName = /^[a-zA-Z0-9/\s+(),-.]{4,}$/;
+export const regExpModelName = /^[a-zA-ZА-яЁё]{4,}[a-zA-ZА-яЁё0-9/\s+(),-.]*$/;
 export const regExpColor = /^[А-яЁё/\s]{3,}$/;
-export const regExpAddress = /^[А-яЁё/\s,0-9"]{3,}$/;
+export const regExpAddress = /^[А-яЁё/\s,.0-9"]{4,}$/;
 export const regExpTab = /\s+/g;
